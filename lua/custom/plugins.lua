e427c8e8f78efed783e07974b45c9f077783850c
Luan Vieira <luanzeba@github.com>
2023-12-08 11:59:24 -0500

    Add copilot plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -19,5 +19,16 @@ local plugins = {
       { "[h", "<Plug>(GitGutterPrevHunk)", desc = "Go to previous hunk" },
     }
   },
+
+  {
+    'github/copilot.vim',
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      vim.cmd([[
+        imap <silent><script><expr> <M-CR> copilot#Accept("\<CR>")
+        let g:copilot_no_tab_map = v:true
+      ]])
+    end,
+  }
 }
 return plugins
