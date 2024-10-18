4b523b099ff9441132187f20fcf86f87af6cab87
Luan Vieira <luanzeba@github.com>
2024-10-18 12:02:52 -0400

    More treesitter syntaxes by default

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,11 +1,12 @@
 local options = {
-  ensure_installed = { "lua" },
+  ensure_installed = { "lua", "markdown", "markdown_inline", "ruby", "go" },
 
   highlight = {
     enable = true,
     use_languagetree = true,
   },
 
+  foldmethod = "manual",
   indent = { enable = true },
 }
 
